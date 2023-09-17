938a3da404b7636354552b751b3cd11160aecd9a
dentopiasa <castelli_philippe@hotmail.com>
2023-09-17 19:39:00 +0300

    minor changes in html and css, set up js DOM connections

---
+++ app.js
@@ -0,0 +1,13 @@
+const btnRock = document.querySelector("btn-rock");
+const btnPaper = document.querySelector("btn-paper");
+const btnScissors = document.querySelector("btn-scissors");
+const btnRestart = document.querySelector("btn-restart");
+
+const playerScore = document.querySelector("player-score");
+const computerScore = document.querySelector("computer-score");
+
+
+
+
+
+
